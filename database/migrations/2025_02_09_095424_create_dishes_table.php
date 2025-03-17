c34241a7e0aeb15304053523f5ee399087de3451
mikihouse1023 <167415128+mikihouse1023@users.noreply.github.com>
2025-03-17 20:45:26 +0900

    Add files via upload

---
+++ database/migrations/2025_02_09_095424_create_dishes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('name', 50);
+            $table->string('val', 10);
+            $table->text('explanation');
+            $table->string('picture', 255);
+            $table->string('genre', 15);
+            $table->timestamps(); // created_at と updated_at を追加
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dishes');
+    }
+};
